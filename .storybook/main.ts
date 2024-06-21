7aaf98990101eac1b96e58c2e7ceb71c21ba04e3
fengnn <fengnn@kmelearning.com>
2024-06-21 16:49:05 +0800

    feat: 修改 package.json 文件

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,6 +14,6 @@ const config: StorybookConfig = {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  staticDirs: ["..\\public"], // 或者 ["../public"] 对于 ubuntu
+  staticDirs: ["../public"],
 };
 export default config;
